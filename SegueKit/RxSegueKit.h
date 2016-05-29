7440fa1ab8b448e3676fc83e8c63ab2ca19d86cb
langyanduan <langyanduan@qq.com>
2016-05-29 23:29:59 +0800

    carthage support

---
+++ SegueKit/RxSegueKit.h
@@ -0,0 +1,20 @@
+//
+//  RxSegueKit.h
+//  RxSegueKit
+//
+//  Created by WuFan on 16/5/29.
+//  Copyright © 2016年 RxSegueKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UIViewController+Swizzle.h"
+
+//! Project version number for RxSegueKit.
+FOUNDATION_EXPORT double RxSegueKitVersionNumber;
+
+//! Project version string for RxSegueKit.
+FOUNDATION_EXPORT const unsigned char RxSegueKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSegueKit/PublicHeader.h>
+
---
+++ SegueKit/SegueKit.h
@@ -0,0 +1,20 @@
+//
+//  SegueKit.h
+//  SegueKit
+//
+//  Created by WuFan on 16/5/29.
+//  Copyright © 2016年 seguekit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UIViewController+Swizzle.h"
+
+//! Project version number for SegueKit.
+FOUNDATION_EXPORT double SegueKitVersionNumber;
+
+//! Project version string for SegueKit.
+FOUNDATION_EXPORT const unsigned char SegueKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SegueKit/PublicHeader.h>
+
